b1571165ff7412ef87690cc6812e56ab8f66bcc6
Turadg Aleahmad <turadg@agoric.com>
2024-11-05 15:39:51 -0800

    build: codegen

---
+++ packages/client-utils/src/codegen/icq/bundle.ts
@@ -0,0 +1,25 @@
+//@ts-nocheck
+import * as _119 from './v1/genesis.js';
+import * as _120 from './v1/icq.js';
+import * as _121 from './v1/packet.js';
+import * as _122 from './v1/query.js';
+import * as _123 from './v1/tx.js';
+import * as _176 from './v1/query.rpc.Query.js';
+import * as _177 from './v1/tx.rpc.msg.js';
+import * as _184 from './rpc.query.js';
+import * as _185 from './rpc.tx.js';
+export namespace icq {
+  export const v1 = {
+    ..._119,
+    ..._120,
+    ..._121,
+    ..._122,
+    ..._123,
+    ..._176,
+    ..._177,
+  };
+  export const ClientFactory = {
+    ..._184,
+    ..._185,
+  };
+}
